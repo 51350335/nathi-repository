73be52407e8d84ba7352eeefc2e506fe1afd80b5
51350335 <nkosinathibongumenzi@gmail.com>
2017-12-06 14:22:29 +0200

    files added
    php and csv

---
+++ svn.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *Converting scv file to an array 
+ */
+function csv_to_array($filename='', $delimiter=',')
+{
+	if(!file_exists($filename) || !is_readable($filename))
+		return FALSE;
+	
+	$header = NULL;
+	$data = array();
+	if (($handle = fopen($filename, 'r')) !== FALSE)
+	{
+		while (($row = fgetcsv($handle, $delimiter)) !== FALSE)
+		{
+			if(!$header)
+				$header = $row;
+			else
+				$data[] = array_combine($header, $row);
+		}
+		fclose($handle);
+	}
+	return $data;
+}
+
+/**
+ * we reading from Rei_251529_datafeed.csv
+ */
+
+print_r(csv_to_array('Rei_251529_datafeed.csv'));
+
+?>
